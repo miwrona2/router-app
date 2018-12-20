7379113f75677e53177649f3e801967ff2542173
Michal Wrona <michal.wrona@fitqbe.com>
2018-12-20 16:46:40 +0100

    tutorial completed

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <nav>Main Template (App.vue)</nav>
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/coins/ethereum">Ethereum</router-link>
+    <router-link to="/coins/bitcoin">Bitcoin</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+            <h1>About</h1>
+        <p>{{ aboutContent }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+    data() {
+        return {
+            aboutContent: 'Some text in about section'
+        }
+    }
+}
+</script>
